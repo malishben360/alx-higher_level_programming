cff651504cf05cc7ff5852166bb70f14c5876137
Elisha Bemjamin <malishben360@gmail.com>
2022-12-02 11:10:52 -0800

    Task 2 0x01 HLP

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',\
-    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',\
-    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+for i in ['a','b','c','d','e','f','g','h','i','j','k','l',
+	'm','n','o','p','q','r','s','t','u','v','w','x','y','z']:
     print("{}".format(i), end="")
