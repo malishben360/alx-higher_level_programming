5718abc8299e0221f156c764e0085699b4981cbe
Elisha Bemjamin <malishben360@gmail.com>
2022-12-03 14:05:41 -0800

    Task 13 0x01 HLP

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+  * insert_node - insert new node in an ordered manner
+  * @head: pionter to the first node of the linked list
+  * @number: Integer for the new node
+  *
+  * Return: Alway point to the new node, or NULL if failed.
+  */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *prev = NULL;
+	listint_t *curr = NULL;
+	listint_t *node = NULL;
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+
+	node->n = number;
+	node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	prev = *head;
+	curr = *head;
+	while (curr)
+	{
+		if (curr->n > number)
+		{
+			prev->next = node;
+			node->next = curr;
+			return (node);
+		}
+		else if (curr->next == NULL)
+		{
+			curr->next = node;
+			return (node);
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+	return (node);
+}
