55386e945bc568e4878c95e54a12bdf6e21df416
Elisha Bemjamin <malishben360@gmail.com>
2022-12-03 05:01:27 -0800

    Task 8 0x01 HLP

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-def _print(char):
+def display(char):
     print("{}".format(char), end="")
 
 
 def uppercase(str):
     for char in str:
         if ord(char) > 96 and ord(char) < 123:
-            _print(chr(ord(char) - 32))
+            display(chr(ord(char) - 32))
         else:
-            _print(char)
-    _print('\n')
+            display(char)
+    display('\n')
