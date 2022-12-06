b4357762c835ad74c98bef4cc5aea46a0bde2344
Elisha Bemjamin <malishben360@gmail.com>
2022-12-05 18:38:24 -0800

    Task 3 0x03 HLP

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list) - 1
-    if length >= 0:
-        for i in range(length, -1, -1):
+    if my_list == None:
+        return None
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
             print("{:d}".format(my_list[i]))
