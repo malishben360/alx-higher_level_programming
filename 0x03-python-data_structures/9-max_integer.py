28405ad0ec1cfe64fcafbf9bf522bb8b01dffd6c
Elisha Bemjamin <malishben360@gmail.com>
2022-12-06 01:52:41 -0800

    Task 9 0x03 HLP

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,5 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    mx_x = 0
-    for x in my_list:
-        if x > mx_x:
-            mx_x = x
-    return mx_x
+    my_list.sort()
+    return my_list[-1]
