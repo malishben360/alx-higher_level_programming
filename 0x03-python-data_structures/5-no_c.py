b4a03911161b0843856a36810a13b0665466da2c
Elisha Bemjamin <malishben360@gmail.com>
2022-12-05 19:20:58 -0800

    Task 5 0x03 HLP

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,10 +2,10 @@
 
 def no_c(my_string):
     new_str = ""
-    l = 0
+    idx = 0
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            new_str += my_string[l:i]
-            l = i + 1
-    new_str += my_string[l:]
+            new_str += my_string[idx:i]
+            idx = i + 1
+    new_str += my_string[idx:]
     return new_str
