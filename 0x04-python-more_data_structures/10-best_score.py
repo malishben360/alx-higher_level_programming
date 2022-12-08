b579a79534db181299215a725a8b66952aec0041
Elisha Bemjamin <malishben360@gmail.com>
2022-12-07 23:34:53 -0800

    Task 10 0x04 HLP

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best_k = ""
+    best_v = 0
+    for k, v in a_dictionary.items():
+        if v > best_v:
+            best_k = k
+            best_v = v
+    return best_k
