32710eda0d3695a715b6cf5d83c2b1bb8e24eb77
Elisha Bemjamin <malishben360@gmail.com>
2022-12-07 19:11:17 -0800

    Task 0 0x04 HLP

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map((lambda mx: list(map((lambda x: x**2), mx))), matrix))
-    return new_matrix
+    new_mx = list(map((lambda mx: list(map((lambda x: x**2), mx))), matrix))
+    return new_mx
