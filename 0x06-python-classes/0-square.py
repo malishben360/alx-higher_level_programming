a02e93abb989f4df7def30d8cfd2d8fbec5c631f
Elisha Bemjamin <malishben360@gmail.com>
2023-01-07 16:39:10 -0800

    Task 0 0x06 HLP

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""The module defines the Square class.
+"""
 
 
 class Square(object):
-    """Class Square that defines a square."""
+    """Class Square that defines a square.
+    """
     pass
