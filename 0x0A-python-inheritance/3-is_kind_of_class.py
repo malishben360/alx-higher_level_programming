d1a83f815ab311e275103beff2f0eb643e205239
Elisha Bemjamin <malishben360@gmail.com>
2023-01-10 10:34:21 -0800

    Task 3 0x0A HLP

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" Compares instances """
+
+
+"""Checks if a given instance is a member of spacified class"""
+
+
 def is_kind_of_class(obj, a_class):
     """ Function that returns True/False if obj is an instance of a_class
 
