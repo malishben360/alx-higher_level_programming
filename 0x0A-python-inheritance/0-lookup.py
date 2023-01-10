72ec5f96df8c8d44412cf6a2762d9f7c007c91d3
Elisha Bemjamin <malishben360@gmail.com>
2023-01-10 07:03:17 -0800

    Task 0 0x0A HLP

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Ananlyses classes and instances of a given class."""
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
